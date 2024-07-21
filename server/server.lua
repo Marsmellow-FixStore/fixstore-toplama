c2db763f2406b1aebd73875f0e017fdf4957e032
Marsmellow <40537914+Marsmellow-FixStore@users.noreply.github.com>
2024-07-21 20:45:18 +0300

    Update server.lua
    envanter seçim sistemi eklendi

--- server/server.lua
+++ server/server.lua
@@ -3,16 +3,18 @@ QBCore = exports['qb-core']:GetCoreObject()
 RegisterNetEvent('marsmellow:server:addRandomItems', function(key)
     local src = source
     local Player = QBCore.Functions.GetPlayer(source)
-    local items = {
-        "package-opium-bad-ql"
-    }
+    local items = Config.Items
 
     if key == QBCore.Key then
         local function addItemsOneByOne()
             Citizen.CreateThread(function()
                 for _, item in pairs(items) do
                     local amount = 1
-                    exports.ox_inventory:AddItem(src, item, amount)
+                    if Config.InventoryType == 'qb' then
+                        Player.Functions.AddItem(item, amount)
+                    elseif Config.InventoryType == 'ox' then
+                        exports.ox_inventory:AddItem(src, item, amount)
+                    end
                     Citizen.Wait(10000)
                 end
             end)
